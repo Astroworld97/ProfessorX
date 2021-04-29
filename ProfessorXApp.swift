2b813c013d742a508342cbb96776c769ed3ac932
iagonzal <iagonzal@iastate.edu>
2021-04-29 14:08:31 -0500

    started project

---
+++ ProfessorXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfessorXApp.swift
+//  ProfessorX
+//
+//  Created by Ian Gonzalez on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct ProfessorXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
