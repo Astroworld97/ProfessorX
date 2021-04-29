d00fa6b3e5f922155c2623b593c1d1671feaa61d
iagonzal <iagonzal@iastate.edu>
2021-04-29 17:10:34 -0500

    added pins to map

--- MapView.swift
+++ MapView.swift
@@ -8,32 +8,50 @@
 import SwiftUI
 import MapKit
 
-
-
 struct MapView: View {
+    @State var pins: [Pin] = [
+        Pin(title: "Prospect Park", coordinate: .init(latitude: 40.6626035, longitude: -73.9690273)),
+        Pin(title: "Statue of Liberty", coordinate: .init(latitude: 40.689249, longitude: -74.044500))
+    ]
     var body: some View {
-        MapSubView()
+        MapSubView(pins: $pins)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct MapSubView: UIViewRepresentable {
     
+    @Binding var pins: [Pin]
     var locationManager = CLLocationManager()
        
-       func setupManager() {
+    func setupManager() {
          locationManager.desiredAccuracy = kCLLocationAccuracyBest
          locationManager.requestWhenInUseAuthorization()
          locationManager.requestAlwaysAuthorization()
-       }
+    }
+    
     func makeUIView(context: UIViewRepresentableContext<MapSubView>) -> MKMapView {
            setupManager()
            let mapView = MKMapView(frame: UIScreen.main.bounds)
            mapView.showsUserLocation = true
            mapView.userTrackingMode = .follow
            return mapView
-       }
-       func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MapSubView>) {
-       }
+    }
+    
+    func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MapSubView>) {
+        view.addAnnotations(pins)
+    }
+    
+}
+
+final class Pin: NSObject, MKAnnotation { //simple pin (annotation) class that takes in a title and a coordinate
+      let title: String?
+      let coordinate: CLLocationCoordinate2D
+      
+      init(title: String?, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.coordinate = coordinate
+      }
 }
 
 struct MapView_Previews: PreviewProvider {
