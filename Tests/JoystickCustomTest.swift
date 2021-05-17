f26b2442f166d4cca26dd0c0443ab48a272cfe58
iagonzal97 <iagonzal97@gmail.com>
2021-05-16 23:11:39 -0500

    [test] made a custom test for the Joystick in a new file called JoystickCustomTest. It's kind of wonky because it is off center but it seems fine otherwise. Requires more attention.

---
+++ Tests/JoystickCustomTest.swift
@@ -0,0 +1,20 @@
+//
+//  JoystickCustomTest.swift
+//  ProfessorX
+//
+//  Created by Ian Gonzalez on 5/16/21.
+//
+
+import SwiftUI
+
+struct JoystickCustomTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JoystickCustomTest_Previews: PreviewProvider {
+    static var previews: some View {
+        JoystickCustomTest()
+    }
+}
