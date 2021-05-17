8b7620c6181ddb91d6b432531a2d6c7462711a14
iagonzal97 <iagonzal97@gmail.com>
2021-05-16 19:04:40 -0500

    [test] added a JoystickTest and ran it successfully.

---
+++ Tests/JoystickTest.swift
@@ -0,0 +1,22 @@
+//
+//  JoystickTest.swift
+//  ProfessorX
+//
+//  Created by Ian Gonzalez on 5/16/21.
+//
+
+import SwiftUI
+import OMJoystick
+
+struct JoystickTest: View {
+    var body: some View {
+        OMJoystick(colorSetting: ColorSetting()) { (joyStickState, stickPosition) in
+    }
+}
+}
+
+//struct JoystickTest_Previews: PreviewProvider {
+//    static var previews: some View {
+//        JoystickTest()
+//    }
+//}
