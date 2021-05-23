9f3a12a8a17d4a65d7b01177da395b166dc8b295
iagonzal97 <iagonzal97@gmail.com>
2021-05-22 19:10:13 -0500

    [test] added comments to clarify usage of centralManagerDidUpdateState(), which is a function of CBCentralManager, which in turn is from the Core Bluetooth library.

--- Tests/CoreBluetoothTest.swift
+++ Tests/CoreBluetoothTest.swift
@@ -14,10 +14,30 @@ import UIKit
  disconnecting from them.
  */
 
-class BluetoothViewController: UIViewController{
+class BluetoothViewController: UIViewController, CBCentralManagerDelegate{
+    
     private var centralManager: CBCentralManager!
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    /*
+     The centralManagerDidUpdateState() method is called by CoreBluetooth whenever the state of Bluetooth on the phone and
+     within the app is updated. Under normal circumstances you should receive a didUpdateState() call to the delegate object
+     almost immediately after initializing the central manager, with the included state being .poweredOn.
+     
+     As of iOS 10, the possible states include:
+     
+     1. poweredOn: Bluetooth is enabled, authorized, and ready for app use.
+     2. poweredOff: The user has toggled Bluetooth off and will need to turn it back on from Settings or the Control Center.
+     3. resetting: The connection with the Bluetooth service was interrupted.
+     4. unauthorized: The user has refused the app permission to use Bluetooth. The user must re-enable it from the app's Settings menu.
+     5. unsupported: The iOS device does not support Bluetooth.
+     6. unknown: The state of the manager and the app's connection to the Bluetooth service is unknown.
+     */
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
         
     }
 }
