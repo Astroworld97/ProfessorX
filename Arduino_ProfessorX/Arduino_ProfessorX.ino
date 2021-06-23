82a96ae84f51ed8b5d4a55712d25b8fc23d41345
iagonzal97 <iagonzal97@gmail.com>
2021-06-22 21:53:48 -0400

    added libraries and code to Arduino_ProfessorX.ino. Added a serial input test that is blank for the moment.

--- Arduino_ProfessorX/Arduino_ProfessorX.ino
+++ Arduino_ProfessorX/Arduino_ProfessorX.ino
@@ -1,11 +1,15 @@
-
 //https://www.raywenderlich.com/2164-arduino-tutorial-integrating-bluetooth-le-and-ios-with-swift
+// Arduino Bluetooth LE Motor Control by iOS
 
-// Arduino Bluetooth LE Servo Controlled by iOS
+#include <SoftwareSerial.h>
 
-int LED = 13;     // Most Arduino boards have an onboard LED on pin 13
+SoftwareSerial BLE_Shield(4,5); //This will create an instance of SoftwareSerial named BLE_Shield. The two parameters are pin numbers, so the BLE shield will use RX and TX lines 4 and 5 respectively.
 
 void setup() {  // Called only once per startup
+
+  BLE_Shield.begin(9600);  // Setup the serial port at 9600 bps. This is the BLE Shield default baud rate. 
+  Serial.begin(9600); //It is important to initialize both Serial and SoftwareSerial. See more details here: https://arduino.stackexchange.com/questions/3996/difference-between-softwareserial-and-serial
+
     // Setup Channel A
   pinMode(12, OUTPUT); //Initiates Motor Channel A pin
   pinMode(9, OUTPUT); //Initiates Brake Channel A pin
@@ -16,32 +20,41 @@ void setup() {  // Called only once per startup
 }
 
 void loop() { // Continuous loop
-  //Motor A forward @ full speed
-  digitalWrite(12, HIGH); //Establishes forward direction of Channel A
-  digitalWrite(9, LOW); //Disengage the Brake for Channel A
-  analogWrite(3, 255); //Spins the motor on Channel A at full speed
 
-  //Motor B backward @ half speed
-  digitalWrite(13, LOW); //Establishes backward direction of Channel B
-  digitalWrite(8, LOW); //Disengage the Brake for Channel B
-  analogWrite(11, 128); //Spins the motor on Channel B at half speed
+// See if new position data is available
+  if (BLE_Shield.available())
+    Serial.write(BLE_Shield.read());
+  if (Serial.available())
+    BLE_Shield.write(Serial.read());
 
-  delay(3000);
-
-  digitalWrite(9, HIGH); //Engage the Brake for Channel A
-  digitalWrite(8, HIGH); //Engage the Brake for Channel B
-
-  delay(1000);
-
-  //Motor A forward @ full speed
-  digitalWrite(12, LOW); //Establishes backward direction of Channel A
-  digitalWrite(9, LOW); //Disengage the Brake for Channel A
-  analogWrite(11, 255); //Spins the motor on Channel B at full speed
-
-  delay(3000);
-
-  digitalWrite(9, HIGH); //Engage the Brake for Channel A
-  digitalWrite(8, HIGH); //Engage the Brake for Channel B
-
-  delay(1000);
+//  //Motor A forward @ full speed
+//  digitalWrite(12, HIGH); //Establishes forward direction of Channel A
+//  digitalWrite(9, LOW); //Disengage the Brake for Channel A
+//  analogWrite(3, 255); //Spins the motor on Channel A at full speed
+//
+//  //Motor B backward @ half speed
+//  digitalWrite(13, LOW); //Establishes backward direction of Channel B
+//  digitalWrite(8, LOW); //Disengage the Brake for Channel B
+//  analogWrite(11, 128); //Spins the motor on Channel B at half speed
+//
+//  delay(3000);
+//
+//  digitalWrite(9, HIGH); //Engage the Brake for Channel A
+//  digitalWrite(8, HIGH); //Engage the Brake for Channel B
+//
+//  delay(1000);
+//
+//  //Motor A forward @ full speed
+//  digitalWrite(12, LOW); //Establishes backward direction of Channel A
+//  digitalWrite(9, LOW); //Disengage the Brake for Channel A
+//  analogWrite(11, 255); //Spins the motor on Channel B at full speed
+//
+//  delay(3000);
+//
+//  digitalWrite(9, HIGH); //Engage the Brake for Channel A
+//  digitalWrite(8, HIGH); //Engage the Brake for Channel B
+//
+//  delay(1000);
 }
+
+// user input practice code
---
+++ serial_input_test/serial_input_test.ino
@@ -0,0 +1,11 @@
+//source: https://forum.arduino.cc/t/serial-input-basics-updated/382007
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
